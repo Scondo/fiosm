5bb0a08484867910ea8937afe06e9fcd8bf4c991
Pavel Zlatovratsky <scondo@mail.ru>
2013-01-23 17:52:53 +0400

    *забытый кусок для отображения информации ФИАС

--- fiosm/__init__.py
+++ fiosm/__init__.py
@@ -9,5 +9,6 @@ def main(global_config, **settings):
     config.add_route('foundbase','found')
     config.add_route('found','found/{guid}/{typ}')
     config.add_route('foundroot','found//{typ}')
+    config.add_route('details','details/{kind}/{guid}')
     config.scan()
     return config.make_wsgi_app()
