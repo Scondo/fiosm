35c2ff5422f2730e646294adf3d65a345c15d68c
Pavel Zlatovratskii <scondo@mail.ru>
2015-12-18 10:04:29 +0300

    Окончательная замена стритманглера на морофологический анализ

--- fiosm/nice_street.py
+++ fiosm/nice_street.py
@@ -6,32 +6,113 @@ Created on 11 нояб. 2013 г.
 '''
 import logging
 import pymorphy2
+import pymorphy2.units
 import numerals
 
 from itertools import chain
-try:
-    import streetmangler
-    locale = streetmangler.Locale('ru_RU')
-    db = streetmangler.Database(locale)
-    db.Load("data/ru_RU.txt")
-    logging.info("Mangle OK")
-except BaseException as e:
-    db = None
-    logging.warn(e.message)
-    logging.warn("Mangle Broken")
+
 
 synonyms = ((u'Первой Маёвки', u'Первой Маевки', u'1 Маевки'),
-            (u'Нижняя Сыромятническая', u'Сыромятническая Ниж.', u'Ниж. Сыромятническая'),
-            (u'Новая Сыромятническая', u'Сыромятническая Нов.', u'Нов. Сыромятническая'),
-            (u'Нижний Сусальный', u'Сусальный Ниж.', u'Ниж. Сусальный'),
-            (u'Верхний Сусальный', u'Сусальный Верхн.', u'Верхн. Сусальный'),
-            (u'Нижний Таганский', u'Ниж. Таганский', u'Таганский Ниж.'),
-            (u'Старый Толмачёвский', u'Толмачевский Ст.', u'Ст. Толмачевский'),
+#            (u'Нижняя Сыромятническая', u'Сыромятническая Ниж.', u'Ниж. Сыромятническая'),
+#            (u'Новая Сыромятническая', u'Сыромятническая Нов.', u'Нов. Сыромятническая'),
+#            (u'Нижний Сусальный', u'Сусальный Ниж.', u'Ниж. Сусальный'),
+#            (u'Верхний Сусальный', u'Сусальный Верхн.', u'Верхн. Сусальный'),
+#            (u'Нижний Таганский', u'Ниж. Таганский', u'Таганский Ниж.'),
+#            (u'Старый Толмачёвский', u'Толмачевский Ст.', u'Ст. Толмачевский'),
             (u'Пруд-Ключики', u'Пруд Ключики'),)
 all_synonyms = set(chain(*synonyms))
+descr = {u'Большой': (u'Бол.', u'Б.'),
+         u'Верхний': (u'Верхн.', u'Верх.', u'В.'),
+         u'Восточный': (u'Вост.', u'В.'),
+         u'Западный': (u'Зап.', u'З.'),
+         u'Левый': (u'Лев.', u'Л.'),
+         u'Малый': (u'Мал.', u'М.'),
+         u'Нижний': (u'Ниж.', u'Н.'),
+         u'Новый': (u'Нов.', u'Н.'),
+         u'Правый': (u'Пр.', u'П.'),
+         u'Северный': (u'Сев.', u'С.'),
+         u'Средний': (u'Ср.', u'С.'),
+         u'Старый': (u'Стар.', u'С.'),
+         u'Южный': (u'Юж.', u'Ю.')}
+
+
 # prepare
 numerals.generate_adj()
+all_num = set(chain(*numerals.adj.items()))
 morph = pymorphy2.MorphAnalyzer()
+suff_morph = pymorphy2.units.KnownSuffixAnalyzer(morph)
+
+
+def desc_all_g():
+    for full, socr in descr.iteritems():
+        yield full
+        f_ = morph.parse(full)[0]
+        yield f_.inflect({'femn'})
+        yield f_.inflect({'neut'})
+        for s in socr:
+            yield s
+desc_all = set(desc_all_g())
+
+
+def get_descr(words, fullname):
+    desc_word = u''
+    if words[0] in desc_all:
+        desc_word = words[0]
+        words = words[1:]
+    elif words[-1] in desc_all:
+        desc_word = words[-1]
+        words = words[:-1]
+    if desc_word.endswith(u'.'):
+        res = set()
+        full_lex = morph.parse(fullname)[0]
+        for full, socr in descr.iteritems():
+            if desc_word in socr:
+                res.update(set(socr))
+                desc_lex = [it for it in morph.parse(full)
+                            if {'ADJF'} in it.tag]
+                res.add(desc_lex[0].inflect({full_lex.tag.gender}).word)
+        # todo: перейти обратно к списку, чтобы полные названия были в начале
+        return (res, words)
+    else:
+        # TODO: Добавить проверку/пропуск на рассогласование
+        return ((desc_word,), words)
+
+
+def get_num(words, fullname):
+    num_word = u''
+    if words[0] in numerals.adj:
+        num_word = words[0]
+        words = words[1:]
+    elif words[-1] in numerals.adj:
+        num_word = words[-1]
+        words = words[:-1]
+    if num_word:
+        # TODO: Добавить проверку/пропуск на рассогласование
+        return ((num_word, numerals.adj[num_word]), words)
+    else:
+        return ((num_word,), words)
+
+
+def get_repr_tag(words):
+    def impossible(tag):
+        if {'Surn', 'nomn'} in tag:
+            return True
+        if {'Name', 'nomn'} in tag:
+            return True
+        return False
+    repr_word = words[-1]
+    #if repr_word.isdigit():
+    #    repr_word = words[-2]
+    lexemes = morph.parse(repr_word)
+    for lex in lexemes:
+        if not impossible(lex.tag):
+            return lex.tag
+    # Default parse contains only impossible lexemes
+    lexemes = suff_morph.parse(repr_word, repr_word.lower(), set())
+    for lex in lexemes:
+        if not impossible(lex[1]):
+            return lex[1]
+
 
 def check_synonym(name):
     if name in all_synonyms:
@@ -51,6 +132,11 @@ def unslash(basename):
     return basename
 
 
+def btr_join(parts, space=u' '):
+    parts = filter(None, parts)
+    return space.join(parts)
+
+
 def nice(basename, shortname, fullname, place=False):
     basename = unslash(basename)
     # Few predefined states (supp. area)
@@ -65,8 +151,6 @@ def nice(basename, shortname, fullname, place=False):
         yield u" ".join((basename, fullname))
     elif fullname == u'край':
         yield u" ".join((basename, fullname))
-    elif fullname == u'чувашия':  # Stupid, but real
-        yield u" ".join((basename, u'Чувашия'))
 
     for name in check_synonym(basename):
         # Check when state part already in name
@@ -75,20 +159,25 @@ def nice(basename, shortname, fullname, place=False):
             basename_ = name
         else:
             words = name.split(u' ')
-            if words[-1] in numerals.adj:
-                logging.info(morph.parse(words[0]))
-                logging.info('ADJF' in morph.parse(words[0])[0].tag)
-                if 'ADJF' in morph.parse(words[0])[0].tag:
-                    yield u' '.join([words[-1], ] + words[:-1] + [fullname, ])
-                    yield u' '.join([numerals.adj[words[-1]], ] + words[:-1] +
-                                    [fullname, ])
-                yield u' '.join([words[-1], ] + [fullname, ] + words[:-1])
-                yield u' '.join([numerals.adj[words[-1]], ] + [fullname, ] +
-                                words[:-1])
+            if len(words) > 1:
+                nums, words = get_num(words, fullname)
+            else:
+                nums = ('',)
+            if len(words) > 1:
+                descs, words = get_descr(words, fullname)
+            else:
+                descs = ('',)
+
+            repr_tag = get_repr_tag(words)
+            if repr_tag is not None:
+                for num in nums:
+                    for desc in descs:
+                        if {'ADJF'} in repr_tag:
+                            yield btr_join([num, desc] + words + [fullname, ])
+                            yield btr_join([num, ] + words + [desc, fullname, ])
+                        else:
+                            yield btr_join([desc, num, fullname] + words)
+                            yield btr_join([num, desc, fullname] + words)
             basename_ = u" ".join((fullname, name))
 
-        if db is not None and not place:
-            ma = db.CheckCanonicalForm(basename_)
-            if ma:
-                basename_ = ma[0]
         yield basename_
